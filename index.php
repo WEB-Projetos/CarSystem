d022781463dc90f3e00729c56679462254a42b2c
lucasamuel <araujo.lucassamuel1@gmail.com>
2021-05-24 22:01:20 -0300

    #2

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Car System</title>
+</head>
+<body>
+    <div class="content">
+        <div class="cont-left">
+            <img src="img/img-login.jpg" alt="">
+        </div>
+        <div class="cont-right">
+            <form action="/" method="post">
+                <div>
+                    <label for="usuario">Usuário:</label>
+                    <input type="text" id="usuario" />
+                </div>
+                <div>
+                    <label for="password">Senha:</label>
+                    <input type="password" id="password" />
+                </div>
+                <div class="login">
+                    <button class="button-login">
+                        <a href="pages/TelaInicial.php">
+                            Entrar
+                        </a>
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
---
+++ pages/TelaInicial.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Car System</title>
+    <link rel="stylesheet" href="/css/telaInicial.css" content="width=device-width, initial-scale=1.0">
+    <script scr= "https://kit.frontawesome.com/a076d05399.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+</head>
+<body>
+
+  <nav class="navbar navbar-light bg-light shadow">
+      <span class="navbar-brand mb-0 h1">
+        <img src="/img//logoc.png" alt="">
+      </span>
+  </nav>
+  </label>
+    <div class="sidebar">
+      <a href="#" class="active">
+        <i class="fas fa-qrcode"></i>
+        <span>COMPRAS</span>
+      </a>
+      <a href="#">
+          <i class="fas fa-link"></i>
+          <span>VENDAS</span>
+      </a>
+      <a href="#">
+        <i class="fas fa-stream"></i>
+        <span>PRODUTOS</span>
+      </a>
+      <a href="#">
+        <i class="fas fa-calendar"></i>
+        <span>CILIENTES</span>
+      </a>
+      <a href="#">
+        <i class="far fa-question-circle"></i>
+        <span>FORNECEDORES</span>
+      </a>
+      <a href="#">
+        <i class="fas fa-sliders-h"></i>
+        <span>FUNCIONARIOS</span>
+      </a>
+    </div>     
+          
+  
+
+    
+</body>
+</html>
