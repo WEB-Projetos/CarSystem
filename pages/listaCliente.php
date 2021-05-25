c51aa9d9d7c3c63cb38d4e61f62b1be2b2882813
lucasamuel <araujo.lucassamuel1@gmail.com>
2021-05-25 13:41:40 -0300

    #5

---
+++ pages/listaCliente.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">   
+    <link rel="stylesheet" href="../css/listaCliente.css">
+    <link rel="icon" type="image/png" href="images/logoc.png"/>
+    <title>Car System</title>
+</head>
+<body>
+    <header class="nav-bar">
+        <div class="hearder">
+            <div class="logo-navbar">
+                <a href="/pages/TelaInicial.php"><img src="../img/logoc.png" alt=""></a>
+                <div class="usuario-navbar">
+                    <a href="#"><img src="../img/user.png" alt=""></a>
+            </div>
+            </div>    
+        </div>
+        <div class="sub-navbar">
+            <div class="sub-navbar-cont">
+                <div class="search-sub">
+                    <a href="#"><img src="../img/search.png" alt=""></a>
+                </div>
+                <div class="sub-titulo">
+                    <h2>CLIENTES</h2>
+                </div>
+                <div class="button-novo">
+                    <a href="">
+                        <img src="../img/plus.svg" alt="">
+                        <h3>ADCIONAR</h3>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="side-bar">
+        <div class="side-bar-lista">
+            <ul>
+                <li>COMPRAS</li>
+                <li>VENDAS</li>
+                <li>PRODUTOS</li>
+                <li>CLIENTES</li>
+                <li>FORNECEDORES</li>
+                <li>FUNCIONÁRIO</li>
+                <li>PROMOÇÕES</li>
+            </ul>
+        </div>
+    </div>
+
+
+    <div class="form-box">
+        <div class="box-titulos">
+            <ul>
+                <li>Nome</li>
+                <li>CPF</li>
+                <li>Editar</li>
+                <li>Remover</li>
+            </ul>
+        </div>
+    </div>
+
+
+    </body>
+</html>
